8e72eb5d7e40e0d5b45bb5a3c8d7f10914d3d48a
chengair <42563475+chengair@users.noreply.github.com>
2018-10-19 15:39:29 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import PIL.ImageFilter,PIL.ImageDraw,PIL.ImageFont
+import random
+import math
+import collections
+import rsa
+import os
+
+key=rsa.newkeys(1024)
+
+privatekey = key[1]#
+publickey = key[0]#
+#print(publickey)
+#print(privatekey)
+
+s='哈哈呵呵！！'
+#bs=bytes(s,'gbk')
+bs=s.encode('utf-8')
+print(s)
+print(bs.decode('utf-8'))
+mes=rsa.encrypt(bs,publickey)
+#print(mes)
+mes=rsa.decrypt(mes,privatekey)
+print(mes)
+print(mes.decode('utf-8'))
+
+#os.system("pause")
